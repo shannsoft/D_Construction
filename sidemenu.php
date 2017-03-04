584849ea237aff402daa590db1d31903aa2bc441
Kajol <kajol121@gmail.com>
2017-03-04 15:44:46 +0530

    4/03/17

--- sidemenu.php
+++ sidemenu.php
@@ -8,7 +8,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel">
         <div class="pull-left image">
-			<img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+			<img src="images/admin.jpg" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p>Admin</p>
